06710b803243efd9a633a8ccabd0d7ef3621cef2
Chris Wood <wchristopherwood912@gmail.com>
2022-10-11 12:51:53 -0700

    finished addStats and addPlayer
    both work as expected

--- input.js
+++ input.js
@@ -1,16 +1,29 @@
-const teamList = []
+const teamList = {}
 const main = document.getElementById("main")
 
 function player(name){
     this.name = name
+
+    this.goals = 0
+    this.shots = 0
+    this.time = 0
 }
 
-function addStat(statName, playerIndex){
-    teamList[playerIndex][1][statName] = 0
-    render()
+function addStatButton(playerName){
+    this.button = document.createElement("input")
+    this.button.setAttribute("id", `${playerName}-add-stat`)
+    this.button.setAttribute("type", "button")
+
+    this.label = document.createElement("label")
+    this.label.setAttribute("for", `${playerName}-add-stat`)
+    this.label.innerHTML = "Add Stat"
 }
 
-function askForStat(){
+function addStat(statName, playerName){
+    teamList[playerName][statName] = 0
+}
+
+function askForStat(playerName){
     const statNameQuery = document.createElement("input")
     statNameQuery.setAttribute("type", "text")
 
@@ -18,15 +31,16 @@ function askForStat(){
 
     statNameQuery.addEventListener("keypress", function (e) {
         if(e.key == "Enter"){
-            addStat(statNameQuery.innerHTML)
+            addStat(statNameQuery.value, playerName)
             statNameQuery.remove()
         }
     })
 }
 
 function addToTeam(player){
-    teamList.push(player)
-    render()
+    teamList[player["name"]] = player
+
+    render(main, renderPlayer(player["name"]))
 }
 
 function askForPlayer() {
@@ -46,40 +60,71 @@ function askForPlayer() {
 const popupArea = document.getElementById("popup")
 
 const addPlayerButton = document.getElementById("add-player")
-// const addStatButton = document.getElementById("add-stat")
+
+const addStatButtons = []
 
 addPlayerButton.addEventListener("click", () => {
     askForPlayer()
 })
 
-// addStatButton.addEventListener("click", () => {
-//     askForStat()
-// })
+function renderStats(name, stats = ["goals", "shots", "time"]){
 
-function render(){
-    for (let i = 0; i < teamList.length; i++) {
-        console.log(teamList[i])
+    const statsElem = document.createElement("section")
+    statsElem.setAttribute("class", "stats")
+    statsElem.setAttribute("id", `${name}-stats}`)
 
-        const player = document.createElement("section")
-        player.setAttribute("class", "player")
 
-        const heading = document.createElement("h2")
-        heading.innerHTML = teamList[i]["name"]
+    for (let j = 0; j < stats.length; j++) {
+        if (j != "name") {
 
-        player.appendChild(heading)
+            const stat = document.createElement("input")
+            stat.setAttribute("type", "number")
+            stat.setAttribute("value", teamList[name][stats[j]])
+            stat.setAttribute("id", `${teamList[name]}-${teamList[name][stats[j]]}`)
+            // stat.setAttribute("class", teamList[i].key(j))
 
-        for (let j in teamList[i]) {
-            if (j != "name") {
-                const stat = document.createElement("input")
-                stat.setAttribute("type", "number")
-                stat.setAttribute("value", teamList[i][j])
+            const statLabel = document.createElement("label")
+            statLabel.setAttribute("for", `${teamList[name]}-${teamList[name][stats[j]]}`)
+            statLabel.innerHTML = `${stats[j]}`
 
-                player.appendChild(stat)
-            }
-            else {
-                continue
-            }
+            statsElem.appendChild(statLabel)
+            statsElem.appendChild(stat)
         }
-        main.appendChild(player)
+        else {
+            continue
+        }
     }
+
+    return statsElem
+
+}
+
+//displays all players and stats for each player
+function renderPlayer(name){
+    console.log(teamList[name])
+
+    const player = document.createElement("section")
+    player.setAttribute("id", teamList[name]["name"])
+    player.setAttribute("class", "player")
+
+    const heading = document.createElement("h2")
+    heading.innerHTML = teamList[name]["name"]
+
+    player.appendChild(heading)
+
+    render(player, renderStats(name))
+    // adds add stat button and label to each player
+    const addStatButtonTemp = new addStatButton(teamList[name]["name"])
+    addStatButtonTemp["button"].addEventListener("click", function () {
+        askForStat(teamList[name]["name"])
+    })
+
+    player.appendChild(addStatButtonTemp["label"])
+    player.appendChild(addStatButtonTemp["button"])
+
+    return player
+}
+
+function render(parentElem, elem){
+    parentElem.appendChild(elem)
 }
